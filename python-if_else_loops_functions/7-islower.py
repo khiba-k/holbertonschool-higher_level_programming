190d7465ce49e6ef8e1c32471143f14b0289a374
khiba-k <khibalpemame@gmail.com>
2024-05-08 12:43:09 +0200

    error handeling

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def islower(c):
+    if isinstance(c, int):
+        traceback.print_exc()
     deci = ord(f"{c}")
     if deci > 96 and deci < 123:
+        print(f"{c} is lower")
         return 1
     else:
+        print(f"{c} is upper")
         return 0
+
+islower(3)
