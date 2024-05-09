bc4e5500d67335b69dff6bf8531f4483fbfd8d67
khiba-k <khibalpemame@gmail.com>
2024-05-09 09:37:10 +0200

    error handling

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,12 @@
 def fizzbuzz():
     num = range(1, 101)
     for x in num:
-        if x % 3 == 0:
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz", end=' ')
+            continue
+        elif x % 3 == 0:
             print("Fizz", end=' ')
         elif x % 5 == 0:
             print("Buzz", end=' ')
-        elif x % 3 == 0 and x % 5 == 0:
-            print("FizzBuzz", end=' ')
         else:
             print(f"{x}", end=' ')
