86ee2ce08ee35f81f7da8a930542d6b1b9158933
khiba-k <you@example.com>
2024-05-09 11:41:13 +0000

    calculator

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add(a, b):
+    c = a + b
+    return c
+
+def sub(a, b):
+    c = a - b
+    return c
+
+def mul(a, b):
+    c = a * b
+    return c
+
+def div(a, b):
+    c = a / b
+    return c
+
