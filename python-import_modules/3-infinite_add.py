2dd3b82f2c0fc278ff95d83e28f878edb14cf17a
khiba-k <you@example.com>
2024-05-10 09:38:04 +0000

    error handling

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,8 +5,11 @@ if __name__ == "__main__":
     res = 0
 
     for x in argv:
-        if x == argv[0]:
+        if x == argv[0] and l == 1:
             print("{}" .format(0))
+            continue
+        elif x == argv[0]:
+            continue
         else:
             i = int(x)
             res += i
