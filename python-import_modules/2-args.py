0cae1c0ea2def29ddf8953dd4a8ce7fdb6aa1367
khiba-k <you@example.com>
2024-05-10 08:35:13 +0000

    error handling

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     l = len(argv)
 
-    if l != 2:
-        print("{} arguments:" .format(l - 1))
-    elif l == 1:
+    if l == 1:
         print("{} arguments." .format(l - 1))
+    elif l != 2:
+        print("{} arguments:" .format(l - 1))
     else:
         print("{} argument:" .format(l - 1))
 
