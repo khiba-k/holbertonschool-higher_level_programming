26bc13a502b1fe99395ce81e22c98c51a3c06988
khiba-k <you@example.com>
2024-05-10 10:40:57 +0000

    error handling

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    
+
     if len(argv) == 1:
         print("{} arguments." .format(len(argv) - 1))
     elif len(argv) != 2:
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    l = len(argv)
     res = 0
 
     for x in argv:
-        if x == argv[0] and l == 1:
+        if x == argv[0] and len(argv) == 1:
             print("{}" .format(0))
             continue
         elif x == argv[0]:
