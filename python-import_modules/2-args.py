592276964e7015607040359f0ffffa0b4eb728ee
khiba-k <you@example.com>
2024-05-10 10:38:57 +0000

    error handlin

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    l = len(argv)
-
-    if l == 1:
-        print("{} arguments." .format(l - 1))
-    elif l != 2:
-        print("{} arguments:" .format(l - 1))
+    
+    if len(argv) == 1:
+        print("{} arguments." .format(len(argv) - 1))
+    elif len(argv) != 2:
+        print("{} arguments:" .format(len(argv) - 1))
     else:
-        print("{} argument:" .format(l - 1))
+        print("{} argument:" .format(len(argv) - 1))
 
     for i, x in enumerate(argv):
         if x == argv[0]:
