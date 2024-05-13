b9e270dbf4a3f86b7cbc6caf6470f573d54bbee9
khiba-k <khibakoenane@gmail.com>
2024-05-13 10:31:55 +0000

    replace element

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    num = len(my_list) - 1
+    if idx < 0 or idx > num:
+        print("{}" .format(my_list))
+    else:
+        my_list[i] = element
+        print"{}" .format(my_list))
