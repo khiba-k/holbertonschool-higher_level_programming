c6c0e047be9187e1b1b3ba5bbb2683d37fd3375d
khiba-k <khibakoenane@gmail.com>
2024-05-13 11:46:39 +0000

    error handling

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     rev = []
-    rev.append(my_list.reverse())
+    rev.extend(my_list.reverse())
     for x in rev:
 
         print("{:d}" .format(x))
