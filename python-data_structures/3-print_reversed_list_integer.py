df24970f26ae5e20d2ecdb957003db98eda1b3ff
khiba-k <khibakoenane@gmail.com>
2024-05-13 11:42:46 +0000

    reverse

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for x in my_list.reverse:
+    rev = []
+    rev.append(mylist.reverse))
+    for x in rev:
+
         print("{:d}" .format(x))
