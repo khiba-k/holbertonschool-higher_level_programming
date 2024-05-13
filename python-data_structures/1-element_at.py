25a17a100a9a0e11e12f02077b6c7fc96d203985
khiba-k <khibakoenane@gmail.com>
2024-05-13 10:53:10 +0000

    error handling

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,4 +4,4 @@ def element_at(my_list, idx):
     if idx < 0 or idx > num:
         return (None)
     else:
-        print("Element at index {:d} is {:d}" .format(idx, my_list[idx]))
+        return my_list[idx]
