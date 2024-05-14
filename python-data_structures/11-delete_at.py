793bb036620088c54f3167d1b66e3e0d2252677a
khiba-k <khibakoenane@gmail.com>
2024-05-14 11:06:33 +0000

    delete item

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    my_list.remove(my_list[idx])
+    if idx < len(my_list) - 1:
+        return my_list
+    else:
+        my_list.remove(my_list[idx])
     return my_list
 
