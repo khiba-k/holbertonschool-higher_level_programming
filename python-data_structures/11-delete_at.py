3a793193c5cb7713f2bd49e916ca4e9c62a05626
khiba-k <khibakoenane@gmail.com>
2024-05-14 11:13:39 +0000

    error handling

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx > len(my_list) - 1:
+    if idx > len(my_list) - 1 or idx < 0:
         return my_list
-    return my_list if idx < 0
     else:
         my_list.remove(my_list[idx])
         return my_list
