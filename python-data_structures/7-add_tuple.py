32a8b24417ca01b9a9c523bd0bc49bed2b923f96
khiba-k <khibakoenane@gmail.com>
2024-05-14 08:59:33 +0000

    add tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    sum1, sum2 = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    if len(tuple_a) == 1:
+        sum1, sum2 = tuple_a[0] + tuple_b[0], 0 + tuple_b[1]
+    elif len(tuple_b) == 1:
+        sum1, sum2 = tuple_a[0] + tuple_b[0], tuple_a[1] + 0
+    else:
+        sum1, sum2 = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+
     new_tuple = (sum1, sum2)
 
     return new_tuple
