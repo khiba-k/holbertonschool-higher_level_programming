ae1c86777ec70a773dd11006b149d2b2229a3926
khiba-k <khibakoenane@gmail.com>
2024-05-14 10:24:26 +0000

    error handling

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,8 @@ def divisible_by_2(my_list=[]):
     new = []
     for x in my_list:
         if x % 2 == 0:
-            new += True
+            new += x
+            return True
         else:
-            new += False
+            new += x
+            return False
