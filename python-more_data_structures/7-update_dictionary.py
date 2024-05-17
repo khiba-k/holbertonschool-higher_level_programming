a40d80ac0c8f6080f4bfe89c7862839ad5b44948
khiba-k <khibakoenane@gmail.com>
2024-05-17 12:54:49 +0000

    update key

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
 
     if key not in a_dictionary:
-        a_dictionary[x] = value
+        a_dictionary[key] = value
 
     for x in a_dictionary:
         if x == key:
