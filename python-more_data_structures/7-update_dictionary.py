507ff0937c332123577a53c18c23a53c93bf8503
khiba-k <khibakoenane@gmail.com>
2024-05-17 11:26:52 +0000

    update key

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for x in a_dictionary:
-        if x == key:
-            a_dictionary[x] = value
-        else:
-            continue
+    def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
 
     return a_dictionary
