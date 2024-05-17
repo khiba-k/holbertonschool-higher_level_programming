42383c6b999eb4784d7b0063ee7a3b570b357608
khiba-k <khibakoenane@gmail.com>
2024-05-17 08:59:04 +0000

    error handling

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-     new_list = []
-    for i in my_list:
-        if i == search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
-    return new_list
+      new = []
+    new.extend(my_list)
+    
+    replaced = False
+    
+    for i in range(len(new)):
+        if new[i] == search:
+            new[i] = replace
+            replaced = True
+
+    if not replaced:
+        return my_list
