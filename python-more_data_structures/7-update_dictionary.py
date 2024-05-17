022aa27198df7a3a7c6cec2c41c85b86263d91b8
khiba-k <khibakoenane@gmail.com>
2024-05-17 11:12:46 +0000

    update key

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     for x in a_dictionary:
         if x == key:
-            a_dictionary.update[x] = value
+            a_dictionary[x] = value
         else:
             return a_dictionary
 
