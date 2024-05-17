4b768ed4828709a353afdf270585632a441c08c6
khiba-k <khibakoenane@gmail.com>
2024-05-17 13:20:07 +0000

    delete key

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,8 +3,8 @@ def multiply_by_2(a_dictionary):
     new = {}
 
     for x in a_dictionary:
-        double = x * 2
+        double = a_dictionary[x] * 2
 
-        new.update({x: double})
+        new.update(x: double)
 
     return new
