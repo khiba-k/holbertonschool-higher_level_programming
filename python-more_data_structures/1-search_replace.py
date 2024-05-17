5c9930926c64fb5932177c9e6826553871711008
khiba-k <khibakoenane@gmail.com>
2024-05-17 07:54:46 +0000

    search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
 
-    my_index = lambda search: mylist.index(search)
-    my_list[my_index] = replace
+    index = my_list.index(search)
+    my_list[index] = replace
+
     return my_list
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
