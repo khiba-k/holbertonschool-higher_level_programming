dcd365b18686189a62591bdfe877e9587c882ef6
khiba-k <khibakoenane@gmail.com>
2024-05-17 08:12:44 +0000

    error handling

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = []
-
     new.extend(my_list)
-    index = new.index(search)
-    new[index] = replace
+
+    if search not in new:
+        return new
+    else:
+        index = new.index(search)
+        new[index] = replace
 
     return new
