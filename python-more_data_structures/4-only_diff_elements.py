11efa2954725edf95f9a795477df79a18e879650
khiba-k <khibakoenane@gmail.com>
2024-05-17 09:50:57 +0000

    elements present in 1 set

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_1, new_2 = [], []
 
-    for x in set_1:
-        if x not in  set_2:
-            new_1.append(x)
-    for y in set_2:
-        if y not in set_1:
-            new_2.append(y)
+    set_3 = set1.difference(set_2)
+    set_4 = set_2.difference(set_1)
 
-    return new_2.extend(new_1)
+    return set3.update(set_4)
