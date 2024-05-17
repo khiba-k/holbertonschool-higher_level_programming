ebe5f14376c80be27daaf2b33b5bd2409e0d9ca0
khiba-k <khibakoenane@gmail.com>
2024-05-17 10:54:54 +0000

    no. of keys

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    alpha = {}
+    alpha = sorted(a_dictionery.keys())
 
-    return sorted(a_dictionery.keys()):
+    for x in alpha:
+        print(f"{key}: {a_dictionary[key]}")
 
