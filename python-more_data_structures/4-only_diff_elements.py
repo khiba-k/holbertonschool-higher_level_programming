c612b2fe56500573c090d180abb1a05bc5a58010
khiba-k <khibakoenane@gmail.com>
2024-05-17 09:55:30 +0000

    elements present in 1 set

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,5 +3,6 @@ def only_diff_elements(set_1, set_2):
 
     set_3 = set_1.difference(set_2)
     set_4 = set_2.difference(set_1)
-
+    
+    my_list = list(set_3.union(set_2))
     return set_3.update(set_4)
