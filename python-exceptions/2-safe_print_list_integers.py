af87bdf7d0a961db9f7e05463933545714721bff
khiba-k <khibakoenane@gmail.com>
2024-05-18 13:20:27 +0000

    print and count integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    printed_integers = 0
-    i = 0
+    print_int = 0
+    for index in range(x):
+        try:
+            print("{:d}".format(my_list[index]), end="")
+        except IndexError:
+            break
+        except (ValueError, TypeError):
+            continue
+        else:
+            print_int += 1
+    print()
+    return print_int
 
-    try:
-        while i < x:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                printed_integers += 1
-            i += 1
-        print ()
-        return printed_integers
-    except IndexError:
-        print("IndexError: list index out of range")
+#!/usr/bin/python3
+safe_print_list_integers = \
+    __import__('2-safe_print_list_integers').safe_print_list_integers
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
