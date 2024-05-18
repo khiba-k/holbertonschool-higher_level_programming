76e5eace7a489d36c99f8b5eb27ecefae9755a6b
khiba-k <khibakoenane@gmail.com>
2024-05-18 12:22:58 +0000

    raise message

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i, length = 0, 0
-    container = ""
 
     for loop in my_list:
         length += 1
     try:
-        if x <= length:
-            while i < x:
-                container += str(my_list[i])
-                i += 1
-            print("{}" .format(container))
-        else:
-            print(f"{my_list}")
-        return length
+        while i < x:
+            print("{}" .format(my_list[i]), end="")
+            i += 1
+        print()
+        return i
     except IndexError:
-            print(f"{my_list}")
-            return length
-
-    return x
+        print()
+        return length
