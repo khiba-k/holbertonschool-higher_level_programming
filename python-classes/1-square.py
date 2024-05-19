c915f1d315a81b2610f08252a1ff0ed26ade991b
khiba-k <khibalpemame@gmail.com>
2024-05-19 10:44:24 +0200

    square size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,6 +4,7 @@
 1-square.py: is a class Square that defines a square
 """
 
+
 class Square:
     """class Square that defines a square
 
@@ -11,5 +12,6 @@ class Square:
         attr1 (size): Size of the Square.
 
     """
+
     def __init__(self, size=0):
         self.__size = size
