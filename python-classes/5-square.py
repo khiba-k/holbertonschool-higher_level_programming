f64d24afe0f529072aa9c9940141f80783a58fdb
khiba-k <khibalpemame@gmail.com>
2024-05-19 12:47:56 +0200

    print square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,7 +6,7 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """Initialize a new square.
 
         Args:
@@ -30,3 +30,11 @@ class Square:
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
