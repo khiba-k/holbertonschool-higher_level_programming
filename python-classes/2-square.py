56e78e4fbc7c1c49f0fcb8b383e34aadc4448bdd
khiba-k <khibalpemame@gmail.com>
2024-05-19 10:59:21 +0200

    raise error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,7 +14,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initializer with default size = 0"""
-        if size not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
