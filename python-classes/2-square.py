02ab6ef591aad04594ddb4df3b2a3fd1981ae2dc
khiba-k <khibalpemame@gmail.com>
2024-05-19 10:58:01 +0200

    raise error

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+1-square.py: is a class Square that defines a square
+"""
+
+
+class Square:
+    """class Square that defines a square
+
+        Attributes:
+        attr1 (size): Size of the Square.
+
+    """
+
+    def __init__(self, size=0):
+        """Initializer with default size = 0"""
+        if size not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
