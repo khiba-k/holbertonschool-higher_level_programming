fc44ba252bd619b704cbd15ed21fa4f4a279d15b
khiba-k <khibakoenane@gmail.com>
2024-05-21 17:20:04 +0000

    error handling

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,25 +27,25 @@ def matrix_divided(matrix, div):
 
     lst = []
 
-    # Check if the matrix is a list of lists of integers/floats
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix) or not all(isinstance(element, (int, float)) for row in matrix for element in row):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    # Check for division by zero
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # Check if div is a number
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
-    # Check if each row has the same size
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-
-    # Perform the division
-    for row in matrix:
+    
+    if div == float('inf') or div == -float('inf'):
+        for row in matrix:
+            new_row = [0.0 for _ in row]
+            lst.append(new_row)
+    else:
+        for row in matrix:
         new_row = []
         for element in row:
             c = round(element / div, 2)
