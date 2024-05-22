ea7860d343cdaf46e30d22cab90d13ce5b2821e5
khiba-k <khibalpemame@gmail.com>
2024-05-22 13:03:07 +0200

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+"""
+    Function divides all elements of a matrix
+
+    Example:
+       first_name = "Khiba"
+       last_name = "Koenane"
+
+       say_my_name(first_name, last_name)
+
+       Khiba Koenane
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+        Args:
+            first_name: first name
+            last_name: second name
+
+        Returns:
+            A sentence with the first and last name
+"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
