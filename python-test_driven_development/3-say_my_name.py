5343714cfc49a8bab7cf5aa5de79212d35e17afc
khiba-k <khibalpemame@gmail.com>
2024-05-22 12:43:35 +0200

    say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if first_name == None:
+        raise ValueError("missing first name")
+    if last_name == None:
+        raise ValueError("missing last name")
+    print("My name is {} {}" .format(first_name, last_name)
