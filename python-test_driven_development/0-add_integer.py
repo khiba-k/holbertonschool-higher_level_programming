d7b7eeaca4a544524ee4ff105174ea0ab2717fe5
khiba-k <khibakoenane@gmail.com>
2024-05-22 19:37:08 +0200

    add integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -23,14 +23,23 @@ def add_integer(a, b=98):
     """
     result = 0
 
-    if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be an integer")
-    elif not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be an integer")
-    elif a is type(float):
-        a = int(a)
-    elif b is type(float):
-        b = int(b)
-    result = a + b
+     if not isinstance(a, (float, int)):
+        if a is not None:
+            raise TypeError("a must be an integer")
+        raise ValueError("a must be an integer")
 
-    return int(result)
+    if not isinstance(b, (float, int)):
+        if b is not None:
+            raise TypeError("b must be an integer")
+        raise ValueError("b must be an integer")
+
+    if type(a) is float and type(b) is float:
+        return int(a) + int(b)
+
+    elif type(a) is not float and type(b) is float:
+        return a + int(b)
+
+    elif type(a) is float and type(b) is not float:
+        return int(a) + b
+
+    return a + b
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -25,9 +25,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if first_name == None:
-        raise ValueError("missing first name")
-    if last_name == None:
-        raise ValueError("missing last name")
 
     print("My name is {} {}" .format(first_name, last_name))
