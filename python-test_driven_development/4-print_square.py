09fe271cde3a3a5210efcc2c4179434b4dbd449d
khiba-k <khibakoenane@gmail.com>
2024-05-23 07:33:30 +0000

    error handling

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -24,9 +24,7 @@ def print_square(size):
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    elif isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-    elif size < 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
     while height < size:
         width = 0
