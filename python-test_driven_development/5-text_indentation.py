1e3ff333e6ddddeca54746e20c7c0de136272cc1
khiba-k <khibakoenane@gmail.com>
2024-05-23 07:53:43 +0000

    error handling

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/bin/usr/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    chars_to_check = ['.', '?', ':']
+    result = []
+    i = 0
+
+     while i < len(text):
+        if text[i] in chars_to_check:
+            result.append(text[i])
+            result.append("\n\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+        else:
+            result.append(text[i])
+            i += 1
+    print("".join(result))
