99c0b50c41a958a91fa0a6b91d21706b1bc6b4fd
khiba-k <khibakoenane@gmail.com>
2024-05-23 07:57:29 +0000

    error handling

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
+"""
+    Function divides all elements of a matrix
+
+    Example:
+       first_name = "Khiba"
+       last_name = "Koenane"
+
+       say_my_name(first_name, last_name)
+
+       Khiba Koenane
+"""
+
+
 def text_indentation(text):
+
+    """
+        Args:
+            first_name: first name
+            last_name: second name
+
+        Returns:
+            A sentence with the first and last name
+    """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
