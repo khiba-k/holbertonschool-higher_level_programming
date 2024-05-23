7de043f0683387d97258b4bc81195bc9041ea6b1
khiba-k <khibakoenane@gmail.com>
2024-05-23 11:55:16 +0000

    text indentation

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -33,7 +33,7 @@ def matrix_divided(matrix, div):
             not all(isinstance(element, (int, float))
                     for row in matrix for element in row)):
         raise TypeError("matrix must be a matrix (list of lists) of"
-                        "integers/floats")
+                        " integers/floats")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -46,7 +46,7 @@ def matrix_divided(matrix, div):
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
         raise TypeError("Each row of the matrix"
-                        "must have the same size")
+                        " must have the same size")
 
     for row in matrix:
         new_row = []
