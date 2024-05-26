e4dc3260a7cbacff8fc8122215bba15fbb726831
khiba-k <khibakoenane@gmail.com>
2024-05-26 09:31:25 +0000

    inherits from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-""" MyList Class
-    Module that creates a class MyList that inherits from list
+"""
+contains the MyList class
 """
 
 
 class MyList(list):
-    """"function that defines a class MyList
-
-
-    """
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """Public instance method that prints the list, in ascending order
-        """
+        """Print a list in sorted ascending order."""
         print(sorted(self))
