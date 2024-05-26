2105aa00802bd0562eda248c3363499d653152f6
khiba-k <khibakoenane@gmail.com>
2024-05-26 21:03:26 +0200

    error handling

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""function that returns True if the object is an instance of a class
-
-
-   that inherited (directly or indirectly) from the specified class;
-   otherwise False.
-"""
+"""Defines a class-checking function."""
 
 
 def inherits_from(obj, a_class):
-    """checks if an object is an instance of a class that inherited
-       from a class directly or indirectly
+    """Check if an object is exactly an instance of a given class.
 
-        Args:
-            obj: object
-            a_class: class
-
-        Returns:
-            True or False
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
     """
-    if isinstance(obj, a_class) is True:
-        return True
-    else:
-        return False
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
