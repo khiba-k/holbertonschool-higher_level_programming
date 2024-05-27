084324b5a2e03d7193cff2e1aee124008006f804
khiba-k <khibakoenane@gmail.com>
2024-05-27 09:45:14 +0000

    write to file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Defines function that writes to txt file"""
+
+
+def write_file(filename="", text=""):
+    """function writes text to file
+    Args:
+        filename: name of file to write to
+        text: text to write
+    Return:
+        returns number of characters
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+        print(text, end="")
+    num = len(text)
+    return num
