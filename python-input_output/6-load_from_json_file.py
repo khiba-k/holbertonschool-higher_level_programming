b3fd5be8be9bd99fe6c9adcad9d693aed0f0d491
khiba-k <khibakoenane@gmail.com>
2024-05-27 12:31:24 +0000

    JSON str to object

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines function that loads object from JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """function creates object from JSON
+    Args:
+        filename: name of file to to load from
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f) 
