b691e8939f5b4b155f2de6dc893c7b92156b0283
khiba-k <khibakoenane@gmail.com>
2024-05-27 09:33:12 +0000

    read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function that reads text file"""
+
+
+def read_file(filename=""):
+    """Function returns a UTF* text file
+    Args:
+        filename: name of file to read
+    Return:
+        UTF8 text file
+    """
+    with open("filename", "r", encoding="utf-8") as f:
+        read_file = f.read()
+        print(read_file)
+    return read_file
