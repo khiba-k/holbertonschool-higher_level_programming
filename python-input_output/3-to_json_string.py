df528097bb19c4167b85f37c559432bede7ab2d6
khiba-k <khibakoenane@gmail.com>
2024-05-27 10:27:31 +0000

    To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a function that a serialzed string"""
+
+
+import json
+def to_json_string(my_obj):
+    """function returns JSON representation of object
+    Args:
+        my_obj: object to serialize
+    Return:
+        serialized object
+    """
+
+    ret = json.dump(my_obj)
+    return ret
