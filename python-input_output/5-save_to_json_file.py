52a34a62327c4e788ea92b81102fda75535b7057
khiba-k <khibakoenane@gmail.com>
2024-05-27 10:58:34 +0000

    To JSON string

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """function object to file as JSON"""
 import json
+import os
 
 
 def save_to_json_file(my_obj, filename):
@@ -12,3 +13,5 @@ def save_to_json_file(my_obj, filename):
 
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
+
+    os.chmod(filename, 0o644)
