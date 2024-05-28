2ba449e001226ff2fa4c5a25b02518917b04bc67
khiba-k <khibalpemame@gmail.com>
2024-05-28 11:14:20 +0200

    error handling

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Adds all arguments to python list"""
+import sys
+
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
