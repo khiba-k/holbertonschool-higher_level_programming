d9a331210568dcd81de3ddfa6dd01a114eb86b56
khiba-k <khibalpemame@gmail.com>
2024-05-28 10:28:06 +0200

    add arguments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-
-ret = load_from_json_file("add_item.json")
-lst = []
-lst.extend(sys.argv[1:])
-save_to_json_file(lst, "add_item.json")
+    try:
+        item = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item = []
+    item.extend(sys.argv[1:])
+    save_to_json_file(item, "add_item.json")
