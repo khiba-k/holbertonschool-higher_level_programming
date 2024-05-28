edff9760449fee13db7737a33390a633f1598f4f
khiba-k <khibalpemame@gmail.com>
2024-05-28 10:26:33 +0200

    add arguments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Adds all arguments to python list"""
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
 
 ret = load_from_json_file("add_item.json")
