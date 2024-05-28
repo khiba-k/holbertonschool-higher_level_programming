065d7c098b7c41074093bd2cb4fd3115f015f3c5
khiba-k <khibalpemame@gmail.com>
2024-05-28 10:20:34 +0200

    add arguments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,6 +3,7 @@
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
+
 ret = load_from_json_file("add_item.json")
 lst = []
 lst.extend(sys.argv[1:])
