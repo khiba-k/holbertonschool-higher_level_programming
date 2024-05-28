4bc74d6ffa94e5cfe28af4b9c16ca4f8c730a7e0
khiba-k <khibalpemame@gmail.com>
2024-05-28 10:42:39 +0200

    class to JSON

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines function that returns dictionary"""
+
+
+def class_to_json(obj):
+    """Function returns dictionary
+
+    Args:
+        obj: instance of class
+    Return:
+        Returns a dictionary
+    """
+    return obj.__dict__
+
+
