787f8450cf67433259634b15e66ddf7d811b4d39
khiba-k <khibakoenane@gmail.com>
2024-05-28 12:25:34 +0000

    pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines class and serializes it"""
+import pickle
+
+
+class CustomObject:
+    """class displays attributes
+    Args:
+        name: string
+        age: integer
+        is_student: boolean
+    """
+    def __init__(self, name, age, is_student):
+        """method initializes object"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """method prints out attributes"""
+        print(f"Name: {name}")
+        print(f"age: {age}")
+        print(f"Is Student: {is_boolean}")
+
+    def serialize(self, filename):
+        """method serializes the current instance of the object"""
+        with open(filename, "wb", encoding="utf-8") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """method returns instance of custom object"""
+        with open(filename, "rb", encoding="utf-8") as f:
+            ret = pickle.load(f)
+        return ret
+
