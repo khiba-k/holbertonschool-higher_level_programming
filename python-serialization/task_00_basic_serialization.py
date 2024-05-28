9af6bd966ade9a5bd067a8affda416ac61603da8
khiba-k <khibakoenane@gmail.com>
2024-05-28 11:17:29 +0000

    basic serialization

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines functions that serialize and deserialize data"""
+import json
+
+
+
+def serialize_and_save_to_file(data, filename):
+    """Function serializes data to JSON
+    Args:
+        data: Python dictionary
+        filename: File name of JSON file
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, filename)
+
+def load_and_deserialize(filename):
+    """Function desirializes JSON data into python dictionery
+    Args:
+        filename: file to read data from
+    Return:
+        returns the python string
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        dictionery = json.load(filename)
+    return dictionery
