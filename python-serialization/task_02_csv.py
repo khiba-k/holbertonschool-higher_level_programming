e53d21e6160cd8f02959f9a6706de7dc06bdf267
khiba-k <khibakoenane@gmail.com>
2024-05-30 08:07:08 +0000

    csv to json

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -10,17 +10,19 @@ def convert_csv_to_json(csv_file):
         csv_file: csv file to convert
         data.json: json file to write to
     """
+
     data = []
     try:
         with open(csv_file, encoding="utf-8") as csvf:
             csvReader = csv.DictReader(csvf)
-            for row in csvf:
+            for row in csvReader:
                 data.append(row)
 
     except FileNotFoundError:
         return False
+
     try:
-        with open(data.json, "w", encoding="utf-8") as f:
+        with open("data.json", "w", encoding="utf-8") as f:
             json.dump(data, f, indent=4)
     except:
         return False
