47bad61bc84a5d126b4e3f83fd52467ada27c59f
khiba-k <khibalpemame@gmail.com>
2024-06-01 15:47:33 +0200

    git web server

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Python web server"""
+import http.server
+import socketserver
+import json
+
+
+PORT = 8000
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            response_data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            response_data = {
+                "status": "OK"
+            }
+            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            response_data = {
+                "error": "Endpoint not found"
+            }
+            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+
+Handler = MyHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
