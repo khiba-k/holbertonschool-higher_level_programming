b35de5b9e6113e9c747245942dc19b73015e03a4
khiba-k <khibakoenane@gmail.com>
2024-06-02 17:36:23 +0000

    Flask Api

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Flask Program"""
+
+
+app = Flask(__name__)
+users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
+}
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route('/data')
+def get_data():
+    usernames = list(users.keys())
+    return jsonify(usernames)
+
+@app.route('/status')
+def status():
+    return "OK"
+
+@app.route('/users/<username>')
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get("username")
+    if username in users:
+        return jsonify({"error": "User already exists"}), 400
+
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
+
+if __name__ == "__main__":
+    app.run()
