6b3b154081ece210eed43505ad8b1a8c41532161
Your Name <you@example.com>
2024-06-23 06:10:31 +0000

    create table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Script creates table
+CREATE TABLE second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table (id, name, score)
+VALUES (id = 1, name="John", score = 10),
+	(id = 1, name="Alex", score = 3),
+	(id = 3, name="Bob", score = 14),
+	(id = 4, name="George", score = 8);
