7a6293b09fbdb209e37767adae700086f8a9ad36
khiba-k <khibakoenane@gmail.com>
2024-06-23 21:45:03 +0200

    create

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- script creates database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
