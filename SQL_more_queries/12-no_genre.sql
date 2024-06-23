20821cea07fa3c7604a33eb3a57b6288ac172641
khiba-k <khibakoenane@gmail.com>
2024-06-23 21:57:16 +0200

    import database dump

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Script imports database dump
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
