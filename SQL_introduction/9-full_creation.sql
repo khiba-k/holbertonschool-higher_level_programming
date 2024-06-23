2f644192596a588a6cfb41d54648e9f2b828a10a
khiba-k <khibakoenane@gmail.com>
2024-06-23 09:23:56 +0200

    create table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS second_table (
 );
 
 INSERT INTO second_table (id, name, score)
-VALUES (1, 'John', 10), (1, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+VALUES (1, "John", 10), (1, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
