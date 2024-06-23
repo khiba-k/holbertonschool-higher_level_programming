f6259f163901c5faaf1240d74a2b5c36009639d2
khiba-k <khibakoenane@gmail.com>
2024-06-23 21:41:56 +0200

    create

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Script creates table force_name
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- script creates table
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256)
+);
