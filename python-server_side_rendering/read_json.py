e18fc4cdfaea62aaddeda8543a824208dbd34e87
khiba-k <khibakoenane@gmail.com>
2024-07-17 15:30:30 +0200

    csv and json

--- python-server_side_rendering/read_json.py
+++ python-server_side_rendering/read_json.py
@@ -6,3 +6,20 @@ def read(filename):
         data = json.load(file)
         values = data.get("items", [])
         return values
+     
+def read_json():
+    with open('products.json') as f:
+        products = json.load(f)
+    return products
+
+def read_csv():
+    products = []
+    with open('products.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            products.append({
+				"id": int(row['id']),
+				"name": row['name'],
+				"category": row['category'],
+				"price": float(row['price'])
+            })
---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+from read_json import read, read_csv, read_json
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+      return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+      return render_template('contact.html')
+
+@app.route('/items')
+def items():
+      
+      file_name = "items.json"
+      items = read(file_name)
+      return render_template('items.html', items=items)
+
+@app.route('/products')
+def display_products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+
+    if source == 'json':
+        products = read_json()
+    elif source == 'csv':
+        products = read_csv()
+    else:
+        return render_template('product_display.html', error="Wrong source")
+    
+    if product_id:
+        products = [product for product in products if str(product['id']) == product_id]
+        if not products:
+            return render_template('product_display.html', error="Product not found")
+	
+    return render_template('product_display.html', products=products)
+
+if __name__ == '__main__':
+       app.run(debug=True, port=5000)
