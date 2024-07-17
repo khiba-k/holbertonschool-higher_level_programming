ae1da8890b5ab0a5b373a0ad82d544df9b512b1a
khiba-k <khibakoenane@gmail.com>
2024-07-17 15:51:07 +0200

    sqlite3

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+from read_json import read, read_csv, read_json, read_db
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+      return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+      return render_template('contact.html')
+
+@app.route('/items')
+def items():
+      
+      file_name = "items.json"
+      items = read(file_name)
+      return render_template('items.html', items=items)
+
+@app.route('/products')
+def display_products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+
+    if source not in ['json', 'csv', 'sql']:
+        return render_template('product_display.html', error="Wrong source")
+
+    if source == 'json':
+        products = read_json()
+    elif source == 'csv':
+        products = read_csv()
+    elif source == 'sql':
+        products = read_db()
+    else:
+        return render_template('product_display.html', error="Wrong source")
+    
+    if product_id:
+        products = [product for product in products if str(product['id']) == product_id]
+        if not products:
+            return render_template('product_display.html', error="Product not found")
+	
+    return render_template('product_display.html', products=products)
+
+if __name__ == '__main__':
+       app.run(debug=True, port=5000)
