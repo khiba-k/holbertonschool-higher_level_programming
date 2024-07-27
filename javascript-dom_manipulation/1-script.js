ba6efb97ddc9ade9bc2ae6d471949795c57d04b6
khiba-k <khibakoenane@gmail.com>
2024-07-27 14:29:32 +0200

    change color onclick & added more files

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+const myHeader = document.body.querySelector("header");
+
+const myClick = document.body.querySelector("red_header");
+myClick.addEventListener(() => {
+    myHeader.style.color = "##FF0000";
+});
